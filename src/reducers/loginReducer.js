df6f356d4b0d87d6af1672e789ffb79a3375252c
Stephen Aranda <aranda.stephen88@gmail.com>
2021-07-30 18:48:03 -0500

    finished signupReducer, need to create actions for signup

---
+++ src/reducers/loginReducer.js
@@ -0,0 +1,21 @@
+// TODO IMPORT ACTIONS
+
+const initialState={
+    login: {},
+    error:'',
+    loading:true
+}
+
+
+export const loginReducer=(state=initialState,action)=>{
+    switch(action.type){
+        case LOGIN_START:
+            return {...state,loading:true}
+        case LOGIN_SUCCESS:
+            return{...state,login:action.payload,loading:false}
+        case LOGIN_FAILURE:
+            return {...state,error:action.payload,loading:false}
+        default:
+            return state;
+    }
+}
---
+++ src/reducers/signupReducer.js
@@ -0,0 +1,28 @@
+//TODO import actions for signup
+import {
+    SIGNUP_START,
+    SIGNUP_FAILURE,
+    SIGNUP_SUCCESS
+} from '../actions/signupActions'
+
+
+
+const initialState={
+    signUp:{},
+    error:'',
+    loading:true
+}
+
+export const signupReducer=(state=initialState,action)=>{
+    switch(action.type){
+        case SIGNUP_START:
+            return {...state,loading:true}
+        case SIGNUP_SUCCESS:
+            return {...state,signup:action.payload,loading:false}
+        case SIGNUP_FAILURE:
+            return {...state,error:action.payload,loading:false}
+        default:
+            return state;
+    }
+
+}
