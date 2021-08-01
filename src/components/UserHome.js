5acc24270d48c830ca3f9204854fd5371fc49794
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2021-08-01 14:34:55 -0400

    set up basic styling for user dashboard

--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -5,39 +5,66 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 const useStyles = makeStyles({
 	root: {
-	  minWidth: 275,
-	  margin: '50px 20px 20px 20px'
+		minWidth: 100,
+		margin: '50px 20px 20px 20px'
 	},
 	title: {
 	textAlign: 'left'
 	},
-	pos: {
-	  marginBottom: 12,
-	},
-	paragraph: {
-		padding: '10px',
-		textAlign: 'left',
-	}
-  });
-
-
-const UserHome = () => {
-	const classes = useStyles();
 	
+	paragraph: {
+		textAlign: 'left'
+	},
+	dashboardContainer: {
+		display: 'flex',
+		justifyContent: 'center'
+	},
+	titleCardContainer: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center'
+	}
+});
+
+
+const UserHome = (props) => {
+	const classes = useStyles();
+
+
+	const handleClick =(page)=>{
+		props.history.push(page);
+	}
 	return(
 		<div>
+		<Card>
+			<CardContent className={classes.titleCardContainer}>
+			<Typography variant='h4' className={classes.title}>Welcome Back</Typography>
+			<AccountCircleIcon style={{fontSize: 60 }} color='primary'/>
+			</CardContent>
+		</Card>
+		
+		<div className={classes.dashboardContainer}>
 		<Card className={classes.root} variant="outlined">
-      <CardContent>
-        <Typography className={classes.title} variant='h4' gutterBottom>My Rentals</Typography>
-		<Typography variant='p' gutterBottom>You currently aren't renting any items. Check out what's for rent by clicking <strong>'View Rentals'</strong> below!</Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">View Rentals</Button>
-      </CardActions>
+		<CardContent>
+			<Typography className={classes.title} variant='h4' gutterBottom>My Rentals</Typography>
+			<Typography variant='p' gutterBottom className={classes.paragraph}>You currently aren't renting any items. Check out what's for rent by clicking <strong>'View Rentals'</strong> below!</Typography>
+		</CardContent>
+		<CardActions>
+        <Button variant='outlined' size="small" onClick={()=>handleClick('/rentals')}>View Rentals</Button>
+		</CardActions>
     </Card>
+
+	<Card className={classes.root} variant="outlined">
+		<CardContent>
+			<Typography className={classes.title} variant='h5' gutterBottom>Pending Requests</Typography>
+			<Typography variant='p' gutterBottom className={classes.paragraph}>You have no pending rental requests!</Typography>
+		</CardContent>
+    </Card>
+	</div>
 	</div>
 	)
 };
